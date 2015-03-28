724c8ee0c87ae1ff25de06b21e175856a796b2b5
Ivan Fraixedes <ifcdev@gmail.com>
2015-03-28 11:21:09 +0000

    finished first impl for travel time lib

--- dev/lib/travel-time-api.js
+++ dev/lib/travel-time-api.js
@@ -2,13 +2,21 @@
 
 let request = require('request-json');
 let moment = require('moment');
-let sortBy = require('lodash').sortBy;
+let _ = require('lodash');
 
+let sortBy = _.sortBy;
+let extend = _.extend;
+
+/* eslint-disable camelcase */
 module.exports = function (appID, appKey) {
   let client = request.createClient('http://api.travelimeapp.com');
   let requiredParams = {
     app_id: appID,
     app_key: appKey
+  };
+
+  return {
+    getRoute: getRoute.bind(null, client, requiredParams)
   };
 };
 
@@ -23,7 +31,20 @@ function getRoute(client, requiredParams, origin, maxTime, destinations) {
     points: destinations
   });
 
+  return Promise(function (resolve, reject) {
   client.post('v3/routes', data, function (err, res, body) {
+    if (err) {
+      reject(err);
+      return
+    }
+
+      if (res.statusCode !== 200) {
+        resolve({
+          statusCode: res.statusCode
+        });
+        return;
+      }
+
     let destsList = Object.keys(body).map(function (id) {
       let dest = body[id];
       dest.id = id;
@@ -31,10 +52,16 @@ function getRoute(client, requiredParams, origin, maxTime, destinations) {
       return dest;
     });
 
-    return sortBy(destsList, 'time').map(function (dest) {
-      d = {};
+    let destinations = sortBy(destsList, 'time').map(function (dest) {
+      let d = {};
       d[dest.id] = dest;
       return d;
     });
+
+    resolve({
+      statusCode: 200,
+      destinations: destinations
+    });
+  });
   });
 }
